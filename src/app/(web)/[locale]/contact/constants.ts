a55e210d9d0bfefead99d655c7db5b6ce1e778c1
Saúl Sáyago <sayagoadd@gmail.com>
2026-02-07 06:31:29 -0600

    fix: update budget options format for consistency

--- src/app/(web)/[locale]/contact/constants.ts
+++ src/app/(web)/[locale]/contact/constants.ts
@@ -1,1 +1,1 @@
-export const BUDGET_OPTIONS = ["<10K", "$10-15K", "$15-30K", "$30-60K", "$60-120K", ">120K"]
+export const BUDGET_OPTIONS = ["<$10K", "$10-15K", "$15-30K", "$30-60K", "$60-120K", ">$120K"]
