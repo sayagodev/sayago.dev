9afe620379633ac6c9c28a7a8fc93bdf0ee57069
Saúl Sáyago <sayagoadd@gmail.com>
2026-02-08 20:03:48 -0600

    feat: add multiple favicon assets and web app manifest
    - Introduced various favicon formats including PNG, SVG, and ICO.
    - Added a web app manifest for improved PWA support.
    - Updated layout to include new favicon links in the head section.

--- src/app/(web)/[locale]/layout.tsx
+++ src/app/(web)/[locale]/layout.tsx
@@ -78,6 +78,13 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale} suppressHydrationWarning data-theme="beish">
+      <head>
+        <link rel="icon" type="image/png" href="/images/favicon/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/images/favicon/favicon.svg" />
+        <link rel="shortcut icon" href="/images/favicon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
+        <link rel="manifest" href="/images/favicon/site.webmanifest" />
+      </head>
       <body
         className={`${[
           monaArgon.variable,
